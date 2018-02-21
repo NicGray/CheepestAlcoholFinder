fb61887515efec3945d8240f678762b58666a16e
NicGray <nic.gray7@gmail.com>
2018-02-21 14:51:25 +1000

    improved site scraping

--- initial tests.py
+++ initial tests.py
@@ -2,30 +2,22 @@ import urllib.request
 import re
 import siteScraping as SS
 #wine
-#url = "https://www.liquorland.com.au/Sparkling/moet-chandon-brut-imperial-nv-piccolo-200ml_5682407"
+url = "https://www.liquorland.com.au/White%20Wine/counting-sheep-marlborough-sauv-blanc-750ml_7805494"
 
 #cider
 #url = "https://www.liquorland.com.au/Beer/somersby-apple-cider-can-375ml-10-pack-_1253180"
 
 #beer
-url = "https://www.liquorland.com.au/Beer/hahn-premium-light-bottle-375ml_380713"
+#url = "https://www.liquorland.com.au/Beer/hahn-premium-light-bottle-375ml_380713"
 
 
 respData = SS.ReadUrl(url)
 
-drinkType = ss.DrinkType(respData)
+drinkType = SS.DrinkType(url)
 
-#name of drank
-
-name = re.findall('>([A-Za-z0-9 \(\)&]+)</li',str(respData))
-
-#alcohol percentage
-
-alcohol = re.findall('>([0-9.%]+)</td',str(respData))
-
-#liquid in case
-liquidCase = re.findall('([0-9]+ x [0-9]+)mL',str(respData))
-liquidCase = liquidCase[0]
-print(name, alcohol, liquidCase)
+if drinkType == "Beer":
+    print(SS.BeerInfo(respData))
+elif drinkType == "White%20Wine":
+    print(SS.WineInfo(respData))
 
 
--- siteScraping.py
+++ siteScraping.py
@@ -1,11 +1,45 @@
 import urllib.request
 import re
 
+#get site contents
 def ReadUrl(url):
     req = urllib.request.Request(url)
     resp = urllib.request.urlopen(req)
     respData = resp.read()
     return respData
-    
-def DrinkType(respData):
-    
+#--------------------------
+
+#get type of drink from url    
+def DrinkType(url):    
+    typeText = re.findall('com.au/([A-Za-z %20]+)/',str(url))
+    return typeText[0]
+#--------------------------
+
+#get informatiopn of beer type drinks
+def BeerInfo(respData):
+    name = GetName(respData)
+    alcohol = GetAlcohol(respData)    
+    amount = re.findall('([0-9]+ x [0-9]+)mL',str(respData))
+    return [name,alcohol,int(amount[0])]
+#--------------------------
+
+#get information on wine type drinks
+def WineInfo(respData):
+    name = GetName(respData)
+    alcohol = GetAlcohol(respData)  
+    amount = re.findall(' ([0-9]+)mL</li',str(respData))
+    print(amount)
+    return [name, alcohol, int(amount[0])]
+#--------------------------
+
+#returns name of product
+def GetName(respData):
+    name = re.findall('>([A-Za-z0-9 \(\)&]+)</li',str(respData))
+    return name[0]
+#--------------------------
+
+#returns alchohol of product as float
+def GetAlcohol(respData):
+    alcohol = re.findall('>([0-9.]+)%</td',str(respData))
+    return float(alcohol[0])
+#--------------------------
